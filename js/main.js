e01c8b0681e6080f775e9642142b8437d1f918d8
Egor Kolev <89554685+Egorkolev@users.noreply.github.com>
2022-04-16 12:15:13 +0300

    product Page
    Add new Product Page

--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
 // поиск елемента в HTML
 let drop_box = document.querySelector('.dropBox');
-console.log(drop_box);
 
 let filter = document.querySelector('.button_filter');
-console.log(filter);
 
 // событие клик (по нажатию на кнопку открывать дроп-бокс)
-filter.addEventListener( "click", function(){
+filter.addEventListener('click', function() {
     drop_box.classList.toggle('open-dropbox');
 });
+
+
+// Product Color choose
+let imgColor = document.querySelectorAll('.imgColor');
+let bodyColor = document.querySelector('.body-color')
+
+imgColor.addEventListener('click', function() {
+    bodyColor.value + imgColor.value;
+})
